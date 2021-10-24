651975f8a1f913a3a68b8ddf716cd63ec3a8da8c
luisangellopez9731 <luisangellopezhernandez1997@hotmail.com>
2021-10-24 18:24:26 -0400

    card component, crud view component

--- src/pages/accounts/Accounts.tsx
+++ src/pages/accounts/Accounts.tsx
@@ -1,10 +1,11 @@
 import { AutoCrud } from "components";
+import { CreateView } from "./create-view";
 
 export const Accounts = () => {
   return (
     <AutoCrud
       TableView={() => <p>Table</p>}
-      CreateView={() => <p>Create</p>}
+      CreateView={CreateView}
       EditView={() => <p>Edit</p>}
     />
   );
---
+++ src/pages/accounts/create-view/CreateView.tsx
@@ -0,0 +1,10 @@
+import { CrudView } from "components/auto-crud";
+import { Card } from "components/card";
+
+export const CreateView = () => {
+  return (
+    <CrudView title="Create Account">
+      <Card>aaa</Card>{" "}
+    </CrudView>
+  );
+};
