eece6444701abefe9c1d64c3c7d8433fadd28cd6
luisangellopez9731 <luisangellopezhernandez1997@hotmail.com>
2021-10-25 11:01:04 -0400

    account create form

--- src/pages/accounts/create-view/CreateView.tsx
+++ src/pages/accounts/create-view/CreateView.tsx
@@ -1,10 +1,32 @@
 import { CrudView } from "components/auto-crud";
 import { Card } from "components/card";
+import { AutoForm, useAutoForm } from "components/auto-form";
 
 export const CreateView = () => {
+  const form = useAutoForm({
+    fields: {
+      name: {
+        type: "text",
+        value: "",
+        label: "Name",
+      },
+      description: {
+        type: "text",
+        value: "",
+        label: "Description",
+      },
+      amount: {
+        type: "number",
+        value: '0',
+        label: "Initial Amount",
+      },
+    },
+  });
   return (
     <CrudView title="Create Account">
-      <Card>aaa</Card>{" "}
+      <Card>
+        <AutoForm form={form} />
+      </Card>{" "}
     </CrudView>
   );
 };
