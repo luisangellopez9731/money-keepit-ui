277cd2dcfca0e16b4275dc28600a71f5dffba6ca
Luis Angel <luisl@azshiptech.com>
2021-11-01 17:18:25 -0400

    fix card border, removing data provider

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -3,10 +3,7 @@ import styles from "./card.module.css";
 
 export const Card: FC = ({ children }) => {
   return (
-    <div
-      className={`${styles.card} bg-white h-full pt-12 p-4`}
-      style={{ borderTopLeftRadius: 60, borderTopRightRadius: 60 }}
-    >
+    <div className={`${styles.card} bg-white h-full pt-12 p-4 rounded-t-3xl`}>
       {children}
     </div>
   );
--- src/core/data-provider/DataProvider.ts
+++
@@ -1,7 +0,0 @@
-export interface DataProvider<T> {
-  getOne(id: string): T;
-  getAll(): T[];
-  create(data: T): string;
-  update(id: string, data: Partial<T>): T;
-  delete(id: string): boolean;
-}
